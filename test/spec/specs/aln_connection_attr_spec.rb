81514cee76dda6e3f9de90fbae8a1cbfa555dcb8
Troy Stribling <troy.stribling@gmail.com>
2007-07-30 03:58:49 +0000

    started work on associations specs

--- test/spec/specs/aln_connection_attr_spec.rb
+++ test/spec/specs/aln_connection_attr_spec.rb
@@ -6,29 +6,3 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "attributes supported by aln_connection" do
 end
 
-#########################################################################################################
-# inheritance relations
-#########################################################################################################
-describe "aln_connection inheritance associations" do
-
-  it "should be able to have descendants" do 
-    AlnConnection.new().should respond_to(:get_descendant)
-  end
-
-  it "should have aln_resource as ancestor" do 
-    AlnConnection.new().should be_descendant_of(:aln_resource)
-  end
-
-end
-
-#########################################################################################################
-# termination associations
-#########################################################################################################
-describe "aln_connection termination associations" do
-
-  it "should be able to have many terminations" do 
-    AlnConnection.new().should respond_to(:aln_terminations)
-  end
-
-end
-
