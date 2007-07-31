02b13ad45d2a495d73cad70817dbfda1b7806811
Troy Stribling <troy.stribling@gmail.com>
2007-07-31 03:58:19 +0000

    finished declare_descendant_association matcher

--- test/rails_root/vendor/plugins/04_rspec_matcher_pac/lib/declare_descendant_association.rb
+++ test/rails_root/vendor/plugins/04_rspec_matcher_pac/lib/declare_descendant_association.rb
@@ -8,19 +8,35 @@ module PlanB
         def matches?(mod)
           @mod = mod
           mod_name = @mod.class.name.tableize.singularize
-          if (
-               @mod.respond_to?(:get_descendant) &&
-               @mod.respond_to?("#{mod_name}_descendant_id".to_sym) &&
-               @mod.respond_to?("#{mod_name}_descendant_type".to_sym)
-              )
-            true
-          else
-            false
+          result = true
+          @err_msg = ""
+          unless (@mod.respond_to?(:get_descendant))
+            @err_msg << "has_descendants not called\n"
+            result = false
           end
+          unless (@mod.respond_to?("#{mod_name}_descendant_id".to_sym))
+            @err_msg << "#{mod_name}_descendant_id not specified\n"
+            result = false
+          else
+            unless (eval("@mod.column_for_attribute('#{mod_name}_descendant_id').type").eql?(:integer))
+              @err_msg << "#{mod_name}_descendant_id not not type :integer\n"
+              result = false
+            end
+          end
+          unless (@mod.respond_to?("#{mod_name}_descendant_type".to_sym))
+            @err_msg << "#{mod_name}_descendant_type not specified\n"
+            result = false
+          else
+            unless (eval("@mod.column_for_attribute('#{mod_name}_descendant_type').type").eql?(:string))
+              @err_msg << "#{mod_name}_descendant_type not type :string\n"
+              result = false
+            end
+          end
+          result
         end
         
         def failure_message
-          "#{@mod.class.name} is unable to support descendant relationships"
+          "#{@mod.class.name} is unable to support descendant relationships\n#{@err_msg}"
         end
   
         def description
