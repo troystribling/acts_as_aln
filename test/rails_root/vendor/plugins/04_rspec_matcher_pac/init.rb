e944c664bcd3c7b22df211e399b9e5b42fe6a35e
Troy Stribling <troy.stribling@gmail.com>
2007-08-03 03:58:29 +0000

    continued work on supporter association spec

--- test/rails_root/vendor/plugins/04_rspec_matcher_pac/init.rb
+++ test/rails_root/vendor/plugins/04_rspec_matcher_pac/init.rb
@@ -3,5 +3,5 @@ require "eql_attribute_value"
 require "eql_attributes"
 require "declare_descendant_association"
 require "declare_ancestor_association"
-require "have_supporter_with_attribute_value.rb"
+require "have_supported_with_attribute_value.rb"
 
