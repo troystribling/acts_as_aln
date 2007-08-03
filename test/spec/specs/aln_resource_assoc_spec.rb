51135307c892fc7e71e6a8bd486134c1764ee3ae
Troy Stribling <troy.stribling@gmail.com>
2007-08-03 03:58:31 +0000

    continued work on supporter association spec

--- test/spec/specs/aln_resource_assoc_spec.rb
+++ test/spec/specs/aln_resource_assoc_spec.rb
@@ -18,29 +18,29 @@ end
 #########################################################################################################
 # supporter association creation and deletion
 ####################################################################@supporter_attr_val#####################################
-describe "aln_supporter association creation and deletion" do
+describe "aln_resource supporting association creation and deletion" do
 
-  it "should be able to add supporter associations for aln_resource models" do 
+  it "should be able to add supported associations for aln_resource models" do 
     root = AlnResource.new(model_data[:aln_resource])
     root << AlnResource.new(model_data[:aln_resource_supported_1])
     root << AlnResource.new(model_data[:aln_resource_supported_2])
-    root.should have_supporter_with_attribute_value(:name, model_data[:aln_resource_supported_1]['name'])
-    root.should have_supporter_with_attribute_value(:name, model_data[:aln_resource_supported_2]['name'])
+    root.should have_supported_with_attribute_value(:name, model_data[:aln_resource_supported_1]['name'])
+    root.should have_supported_with_attribute_value(:name, model_data[:aln_resource_supported_2]['name'])
   end
 
-  it "should be able to remove a specified supporter association for aln_resource models" do 
+  it "should be able to remove a specified supported association for aln_resource models" do 
   end
 
-  it "should be able to remove all supporter associations for aln_resource models" do 
+  it "should be able to remove all supported associations for aln_resource models" do 
   end
 
-  it "should be able to add supporter associations for aln_resource decsendant models" do 
+  it "should be able to add supported associations for aln_resource decsendant models" do 
   end
 
-  it "should be able to remove a specified supporter association for aln_resource descendant models" do 
+  it "should be able to remove a specified supported association for aln_resource descendant models" do 
   end
 
-  it "should be able to remove all supporter associations for aln_resource descendant models" do 
+  it "should be able to remove all supported associations for aln_resource descendant models" do 
   end
 
 end
