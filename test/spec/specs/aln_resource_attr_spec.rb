d76bb03e70505c94c20d878309a66e1e056f4e89
Troy Stribling <troy.stribling@gmail.com>
2007-08-20 03:57:50 +0000

    continued work an aln_resource associations

--- test/spec/specs/aln_resource_attr_spec.rb
+++ test/spec/specs/aln_resource_attr_spec.rb
@@ -38,24 +38,16 @@ end
 #########################################################################################################
 describe "attribute identifying support hierarchy depth" do
 
-  before(:all) do
-    @r = AlnResource.new(:name => model_data[:aln_resource][:name])
-    @r.save
-  end
-
-  after(:all) do
-    @r.destroy
-  end
-
   before(:each) do
+    AlnResource.new(:name => model_data[:aln_resource][:name]).save
     @root = AlnResource.find_support_root_by_model(AlnResource, :first)
   end
 
   after(:each) do
-    @root.save
+    AlnResource.find_support_root_by_model(AlnResource, :first).destroy
   end
 
-  it "should be 0 for are no supported" do
+  it "should be 0 for no supported" do
     @root.depth.should eql(0)
   end
 
@@ -66,24 +58,34 @@ describe "attribute identifying support hierarchy depth" do
   end
 
   it "should remain 1 as additional supported with no supported are added" do
+    @root.depth.should eql(0)
+    @root << AlnTermination.new(model_data[:aln_termination_supported_1])
     @root.depth.should eql(1)
     @root << AlnTermination.new(model_data[:aln_termination_supported_1])
     @root.depth.should eql(1)
   end
 
   it "should remain 1 when a supported with no supported is destroyed and more than 1 supported with no supported remain" do
+    @root.depth.should eql(0)
+    @root << AlnTermination.new(model_data[:aln_termination_supported_1])
+    @root.depth.should eql(1)
+    @root << AlnTermination.new(model_data[:aln_termination_supported_1])
     @root.depth.should eql(1)
     @root.destroy_supported(AlnTermination, :first, :conditions => "aln_resources.name = '#{model_data[:aln_termination_supported_1]['name']}'")
     @root.depth.should eql(1)
   end
 
   it "should become 0 if all supported are destroyed" do
+    @root.depth.should eql(0)
+    @root << AlnTermination.new(model_data[:aln_termination_supported_1])
+    @root.depth.should eql(1)
+    @root << AlnTermination.new(model_data[:aln_termination_supported_1])
     @root.depth.should eql(1)
     @root.clear_supported
     @root.depth.should eql(0)
   end
 
-  it "should increment by 1 for every layer of supported added" do
+  it "should increment by 1 when another layer of supported is added" do
     @root.depth.should eql(0)
     @root << AlnTermination.new(model_data[:aln_termination_supported_1])
     @root << AlnTermination.new(model_data[:aln_termination_supported_1])
