19b67e48788605cd61c52d3590ae5bb921c1c59f
Troy Stribling <troy.stribling@gmail.com>
2007-10-23 03:57:35 +0000

    continued rewrite of support hierarchy

---
+++ lib/aln_supporter.rb
@@ -0,0 +1,51 @@
+####################################################################################
+#### ALN Resource Supporter container
+####################################################################################
+class AlnSupporter
+
+  ##################################################################################
+  def initialize(sup)
+    @supported = sup
+    @supporter = nil
+    @loaded = false
+  end
+  
+  ##################################################################################
+  def method_missing(meth, *args, &blk)
+    begin
+      super
+    rescue NoMethodError
+      @supporter.send(meth, *args, &blk)
+    end
+  end
+
+  ##################################################################################
+  def loaded?
+    @loaded
+  end
+
+  ##################################################################################
+  def exists?
+    not @supporter.nil?
+  end
+
+  ##################################################################################
+  def id
+    @supporter.nil? ? nil : @supporter.id
+  end
+
+  ##################################################################################
+  def eql?(val)
+    @supporter.eql?(val)
+  end
+  
+  ##################################################################################
+  def load
+    unless loaded? or @supporter.nil?
+      @supporter = @supported.class.find(@supported.supporter_id)
+      @loaded = true
+    end
+    self
+  end
+  
+end
